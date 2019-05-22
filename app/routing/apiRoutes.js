efc4ff634c4197a2635ad0563350ef458554384a
Dongdong Zhang <45675253+DZhangCoder@users.noreply.github.com>
2019-05-22 12:17:57 -0700

    updating routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,19 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+// ===============================================================================
+// LOAD DATA
+var friendsData = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
+  app.post("/api/friends", function (req, res) {
+    var newfriend = req.body;
+    console.log(newfriend);
+    friends.push(newfriend);
+    res.json(newfriend);
+  });
+}; 
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Routes
-// =============================================================
-
-// Displays all friends in json format 
-app.get("/api/friends", function (req, res) {
-  return res.json(friends);
-});
-
-app.post("/api/friends", function (req,res){
-  var newfriend = req.body; 
-  console.log(newfriend);
-  friends.push(newfriend); 
-  res.json(newfriend); 
-}); 
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,37 +1,21 @@
-// Dependencies
-// =============================================================
-var express = require("express");
 var path = require("path");
+module.exports = function (app) {
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+  // Routes
+  // =============================================================
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // Route that sends the user to the survey Page
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
-// Routes
-// =============================================================
+  // Route that sends the user to the home page
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
-// Route that sends the user to the survey Page
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
-
-// Route that sends the user to the home page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-//This route will redirect any route not handled to the index "/"
-app.get('*',function (req, res) {
-  res.redirect('/');
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+  //This route will redirect any route not handled to the index "/"
+  app.get('*', function (req, res) {
+    res.redirect('/');
+  });
+}
