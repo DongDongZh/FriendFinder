cb4d1cf27ed366074ca839a6d4adca81d014e079
Dongdong Zhang <45675253+DZhangCoder@users.noreply.github.com>
2019-05-22 14:58:49 -0700

    updating routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,15 @@ var friendsData = require("../data/friends");
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     res.json(friendsData);
+    for (var i =0; i<friendsData.length;i++){
+      var totalScore = friendsData[i].scores; 
+      console.log(totalScore);
+    }
   });
   app.post("/api/friends", function (req, res) {
     var newfriend = req.body;
     console.log(newfriend);
-    friends.push(newfriend);
+    friendsData.push(newfriend);
     res.json(newfriend);
   });
 }; 
